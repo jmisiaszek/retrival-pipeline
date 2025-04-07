1b2db38f4ae1a0428aebb45be66d3c22eb44b17d
Jakub <jmisiaszek3@gmail.com>
2025-04-07 21:19:36 +0200

    Metrics and result analisys

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # retrival-pipeline
 A task for JetBrains Internship recrutation
+
+A pipeline for chunk information retrival and quality metrics.
+
+## Files
+
+Quick description of souce files in this project
+
+- `trimCSV.py` Used to trim file with questions to only those related to present corporas.
+- `FixedTokenChunker.py` Implementation of the chunker.
+- `chunk_eval.py` Implementation of the pipeline.
+
+## How to run
+
+Install all necessary dependencies using 
+
+```
+pip install -r requirements.txt
+```
+
+and then run the script using 
+
+```
+python3 chunk_eval.py > result
+```
+
+## Results and summary
+
+Here's the table with results that I got:
+
+| Parameters                  | Mean Precision          | Std Precision         | Mean Recall            | Std Recall             |
+|-----------------------------|-------------------------|-----------------------|------------------------|------------------------|
+| chunk_size=400, chunk_num=10 | 0.0018051932349533778    | 0.005289221104898678   | 0.1202822079877414      | 0.3274033320325772      |
+| chunk_size=400, chunk_num=5  | 0.009132474129943615     | 0.018678100316375578   | 0.28664758283912994     | 0.4737632880451287      |
+| chunk_size=200, chunk_num=10 | 0.011973157208216388     | 0.02122492200450722    | **0.4030443037628191**      | 0.5633036074303283      |
+| chunk_size=200, chunk_num=5  | **0.021467239166554575**     | 0.034981500455698804   | 0.35135880395593744     | 0.4646206938649207      |
+
+Intuitively, reducing the chunk size resulted in higher mean Precision value. This comes from less text being selected overall. What also plays in to this score is golden excerpts being quite short on average.
+
+The unexpected higher Recall in second row in comparison to the first might come from embedding model. I looked further in the results and indeed some chunks in second run had higher intersection than in the first.
+
+There's a tradeoff between Precision and Recall, which comes from including more or less text in the context. This is only visible in row 4, where the Recall value started to fall down. This might mean that we crossed the line, and further lowering of both parameters might worsen the Recall even more.
--- chunk_eval.py
+++ chunk_eval.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import json
 from sklearn.metrics.pairwise import cosine_similarity
 from typing import (
     Any,
@@ -16,7 +17,7 @@ def find_edges(chunks: List[str], corpus: str):
     for chunk in chunks:
         start = corpus.find(chunk)
         if start == -1:
-            raise Exception("Chunk not in the corpus.")
+            raise Exception(f"Chunk not in the corpus: {chunk}")
         end = start + len(chunk)
         edges.append({"start": start, "end": end})
     return edges
@@ -51,6 +52,36 @@ def get_num_chunks(question: str,
     top_chunks = [collection[idx] for idx in top_num]
     return top_chunks
 
+def eval_question(reference_coll: List[Dict[str, Any]], 
+                  chunk_coll: List[Dict[str, Any]],
+                  corpus: str):
+    
+    size_retrived = 0
+    for chunk in chunk_coll:
+        size_retrived += len(chunk["chunk"])
+    
+    size_references = 0
+    for chunk in reference_coll:
+        size_references += len(chunk["content"])
+
+    chunk_sorted = sorted(chunk_coll, key=lambda x: x["start"])
+    for i in range(len(chunk_coll) - 1):
+        chunk_sorted[i]["end"] = min(chunk_coll[i]["end"], chunk_coll[i + 1]["start"])
+
+    size_intersection = 0
+    for chunk in chunk_sorted:
+        for reference in reference_coll:
+            start = max(chunk["start"], reference["start_index"])
+            end = min(chunk["end"], reference["end_index"])
+            if start < end:
+                size_intersection += end - start
+
+    precision = size_intersection / size_retrived
+    recall = size_intersection / size_references
+
+    return precision, recall
+
+
 def evaluate(chunker: TextSplitter, 
              embed_fn: Callable[[List[str]], List[float]], 
              chunk_num: int):
@@ -63,7 +94,7 @@ def evaluate(chunker: TextSplitter,
         chunk_num: Number of top chunks to retrive.
     """
     # I picked "wikitexts.md" as my corpus because it has the biggest size.
-    corpus_file = open("./corpora/wikitexts.md", 'r')
+    corpus_file = open("./corpora/wikitexts.md", 'r', encoding="utf-8")
     corpus = corpus_file.read()
     questions = open("./questions_df_trimmed.csv", 'r')
     
@@ -74,26 +105,46 @@ def evaluate(chunker: TextSplitter,
 
     q_reader = csv.DictReader(questions)
 
+    precisions = []
+    recalls = []
+
     for row in q_reader:
         if row["corpus_id"] == "wikitexts":
             top_chunk = get_num_chunks(row["question"], collection, embed_fn, chunk_num)
 
+            precision, recall = eval_question(json.loads(row["references"]), top_chunk, corpus)
+            precisions.append(precision)
+            recalls.append(recall)
 
-
-            print(row["question"], "\n")
-            for chunk in top_chunk:
-                print(chunk["chunk"], "\n")
-            print("----\n")
-            
-
+    print(f"Mean precision: {np.mean(precisions)}")
+    print(f"Std precision: {np.std(precisions)}")
+    print(f"Mean recall: {np.mean(recalls)}")
+    print(f"Std recalls: {np.std(recalls)}")
 
 if __name__ == "__main__":
-    chunker = FixedTokenChunker(chunk_size=400, chunk_overlap=20)
+    # Chunk overlap will be around 5% of the chunk size
+    chunker_400 = FixedTokenChunker(chunk_size=400, chunk_overlap=20)
+    chunker_200 = FixedTokenChunker(chunk_size=200, chunk_overlap=10)
+    chunker_100 = FixedTokenChunker(chunk_size=100, chunk_overlap=6)
 
     def calculate_embeds(chunks: List[str]) -> List[float]:
         """ Calculate embeddings for chunks. """
         embed_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
         embeds = embed_model.encode(chunks)
         return embeds
-        
-    evaluate(chunker, calculate_embeds, 10)
+    
+    print("Evaluation of FixedTokenChunker")
+
+    print("\nParams: chunk_size=400, chunk_num=10\n")
+    evaluate(chunker_400, calculate_embeds, 10)
+
+    print("\nParams: chunk_size=400, chunk_num=5\n")
+    evaluate(chunker_400, calculate_embeds, 5)
+
+    print("\n")
+
+    print("\nParams: chunk_size=200, chunk_num=10\n")
+    evaluate(chunker_200, calculate_embeds, 10)
+
+    print("\nParams: chunk_size=200, chunk_num=5\n")
+    evaluate(chunker_200, calculate_embeds, 5)
